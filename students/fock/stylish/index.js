5009a6e3239aa0104d14fe1acbe9dd051f1ce909
Fock Space <a86gj3sp4@gmail.com>
2023-02-09 14:34:13 +0800

    setup server

--- students/fock/stylish/index.js
+++ students/fock/stylish/index.js
@@ -8,5 +8,6 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
-  console.log(`Process id: ${process.pid}`);
+  console.log(`you can access the server with the link: http://54.148.100.118`);
+  console.log(`kill this process with: $kill ${process.pid}`);
 });
