2ab71a19bf1ec0b706e1514aec34f408d1394ec4
Fock Space <a86gj3sp4@gmail.com>
2023-02-10 13:37:32 +0800

    [set] set environment var

--- students/fock/stylish/app.js
+++ students/fock/stylish/app.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -8,6 +9,8 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
-  console.log(`you can access the server with the link: http://35.89.81.174:8000/`);
+  console.log(
+    `you can access the server with the link: http://35.89.81.174:8000/`
+  );
   console.log(`kill this process with:$ kill ${process.pid}`);
 });
