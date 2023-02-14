79711fcddd1672377c94db19d2d32f3d8aae3da4
Fock Space <a86gj3sp4@gmail.com>
2023-02-14 12:20:51 +0800

    [DB] setup backup.sql for dump

--- students/fock/stylish/db/backup.sql
+++ students/fock/stylish/db/backup.sql
@@ -16,6 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'men'),(2,'women'),(3,'accessories');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `product`
 --
 
@@ -23,10 +47,19 @@ DROP TABLE IF EXISTS `product`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `product` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `category_id` int NOT NULL,
+  `sub_category_id` int NOT NULL,
   `title` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `description` text NOT NULL,
+  `main_image` varchar(255) DEFAULT NULL,
+  `other_images` text,
+  PRIMARY KEY (`id`),
+  KEY `category_id` (`category_id`),
+  KEY `sub_category_id` (`sub_category_id`),
+  CONSTRAINT `product_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
+  CONSTRAINT `product_ibfk_2` FOREIGN KEY (`sub_category_id`) REFERENCES `sub_category` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,7 +68,67 @@ CREATE TABLE `product` (
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,2,1,'ad','sadsa','uploads/stylish product (2).png-1676343091003','[\"uploads/stylish product (2).png-1676343091004\",\"uploads/stylish product (1).png-1676343091006\",\"uploads/stylish product.png-1676343091008\"]');
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_item`
+--
+
+DROP TABLE IF EXISTS `product_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product_item` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `product_id` int NOT NULL,
+  `SKU` varchar(255) DEFAULT NULL,
+  `stock_qty` int NOT NULL DEFAULT '0',
+  `price` decimal(10,0) NOT NULL,
+  `color` varchar(20) NOT NULL,
+  `size` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `product_id` (`product_id`),
+  CONSTRAINT `product_item_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_item`
+--
+
+LOCK TABLES `product_item` WRITE;
+/*!40000 ALTER TABLE `product_item` DISABLE KEYS */;
+INSERT INTO `product_item` VALUES (1,1,NULL,321,23,'#FFFF00','S'),(2,1,'#FFFF00S',321,23,'#FFFF00','S'),(3,1,'#FFFF00S',321,23,'#FFFF00','S'),(4,1,'#FF0000M',321,213,'#FF0000','M'),(5,1,'#FF0000M',321,213,'#FF0000','M'),(6,1,'#FF0000M',321,213,'#FF0000','M'),(7,1,'#FF0000M',321,213,'#FF0000','M'),(8,1,'#FF0000M',321,213,'#FF0000','M');
+/*!40000 ALTER TABLE `product_item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sub_category`
+--
+
+DROP TABLE IF EXISTS `sub_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sub_category` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `texture` varchar(255) NOT NULL,
+  `wash` varchar(255) NOT NULL,
+  `place` varchar(255) NOT NULL,
+  `note` varchar(255) NOT NULL,
+  `story` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sub_category`
+--
+
+LOCK TABLES `sub_category` WRITE;
+/*!40000 ALTER TABLE `sub_category` DISABLE KEYS */;
+INSERT INTO `sub_category` VALUES (1,'2143','32121','3213','32132','132123');
+/*!40000 ALTER TABLE `sub_category` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -47,4 +140,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-02-08 19:43:17
+-- Dump completed on 2023-02-14 12:20:28
