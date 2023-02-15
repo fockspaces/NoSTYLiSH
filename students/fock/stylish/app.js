69ea7cea1dc4ac8dd051d746b0e24a7751f81ddc
Fock Space <a86gj3sp4@gmail.com>
2023-02-15 15:14:50 +0800

    [server/err] catch 404 page when there's no match url to api

--- students/fock/stylish/app.js
+++ students/fock/stylish/app.js
@@ -26,6 +26,9 @@ const admin = require("./routes/admin");
 app.use("/api/1.0/products", product);
 app.use("/admin", admin);
 app.use("/images/", express.static("./uploads/"));
+app.use((req, res, next) => {
+  res.status(404).send({ err: "The requested resource could not be found" });
+});
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
