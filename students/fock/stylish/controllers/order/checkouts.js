513d15e7c1b9766a03debb631ff465cfc7ac7d53
Fock Space <a86gj3sp4@gmail.com>
2023-02-21 13:38:57 +0800

    [checkout] setup the prototype for order creating process

--- students/fock/stylish/controllers/order/checkouts.js
+++ students/fock/stylish/controllers/order/checkouts.js
@@ -1,34 +1,43 @@
 const { orderCreate } = require("./orderCreate");
-const { validateOrder } = require("../../utils/inputValidation");
 const { TapPay } = require("../../utils/tappay");
-const { GetRecipient, updateOrder } = require("../../models/Order/Order");
+const {
+  GetRecipient,
+  updateOrder,
+  itemListCreate,
+} = require("../../models/Order/Order");
 
 const renderCheckoutPage = (req, res) => {
   return res.render("orders/checkout");
 };
 
 const checkoutHandler = async (req, res) => {
+  // todo :
   // fetch each item in list
 
-  // check the item is enougn in the inventory
+  // check the item is enough in the inventory
 
   // update inventory
 
   // fetch order from body and check
   const { order } = req.body;
-  const inValids = validateOrder(order);
-  if (inValids.length)
-    return res.status(400).send({ err: "missing values", inValids });
 
   // create order
   const orderId = await orderCreate(order);
+  if (!orderId)
+    return res
+      .status(400)
+      .send({ err: "missing some values while creating order" });
 
   // create item_lists
+  const { list } = req.body;
+  if (!list.length) return res.status(400).send({ err: "lack of items" });
 
-
+  const listResult = await itemListCreate(orderId, list);
+  if (!listResult)
+    return res.status(400).send({ err: "items inserting failed" });
 
   // fetch recipient
-  const recipient = await GetRecipient(order.recipient_id);
+  const recipient = await GetRecipient(order.recipient);
   if (!recipient) return res.status(404).send({ err: "recipient not found" });
 
   // prime error handling
@@ -51,8 +60,7 @@ const checkoutHandler = async (req, res) => {
     return res.status(400).send({ err: "error on update order" });
 
   // failed, give back items from item_list to inventory
-  
-  
+
   // otherwise, send error
   return res.status(200).send({ data: { number: orderId } });
 };
--- students/fock/stylish/models/Order/Order.js
+++ students/fock/stylish/models/Order/Order.js
@@ -1,14 +1,19 @@
 const pool = require("../../utils/pool");
 
 const AddOrder = async (order) => {
-  const { shipping, payment, subtotal, freight, total, recipient_id } = order;
-  const insertOrder = `INSERT INTO orders (shipping, status, payment, subtotal, freight, total, recipient_id)
+  try {
+    const { shipping, payment, subtotal, freight, total, recipient } = order;
+    const insertOrder = `INSERT INTO orders (shipping, status, payment, subtotal, freight, total, recipient_id)
   VALUES (?, 'unpaid', ?, ?, ?, ?, ?)`;
 
-  const values = [shipping, payment, subtotal, freight, total, recipient_id];
+    const values = [shipping, payment, subtotal, freight, total, recipient];
 
-  const [result] = await pool.query(insertOrder, values);
-  return result.insertId;
+    const [result] = await pool.query(insertOrder, values);
+    return result.insertId;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
 };
 
 const GetRecipient = async (id) => {
@@ -25,10 +30,25 @@ const updateOrder = async (id) => {
   return result.affectedRows === 1;
 };
 
-const AddToItemList = async (orderId, itemId, qty) => {
-  
-}
+const itemListCreate = async (orderId, list) => {
+  const connection = await pool.getConnection();
+  try {
+    await connection.beginTransaction();
+    for (const { product_item_id, qty } of list) {
+      const addItemList =
+        "INSERT INTO item_lists (order_id, product_item_id, qty) VALUES (?,?,?)";
+      const values = [orderId, product_item_id, qty];
+      await pool.query(addItemList, values);
+    }
+    await connection.commit();
+    return true;
+  } catch (e) {
+    await connection.rollback();
+    console.error("Error executing transaction", e);
+    return false;
+  } finally {
+    connection.release();
+  }
+};
 
-
-
-module.exports = { AddOrder, GetRecipient, updateOrder };
+module.exports = { AddOrder, GetRecipient, updateOrder, itemListCreate };
