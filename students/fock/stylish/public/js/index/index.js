c01b6f847af65fe8794397aaad7b782620e7eaab
Fock Space <a86gj3sp4@gmail.com>
2023-02-22 13:55:56 +0800

    [index] finish the feature to redirect with query_string and by navbar link

--- students/fock/stylish/public/js/index/index.js
+++ students/fock/stylish/public/js/index/index.js
@@ -15,28 +15,43 @@ const fetchDataBycategory = async (category, paging) => {
   }
 };
 
-// Call the fetchData() function
-fetchDataBycategory("all").then((data) => {
-  const products = data.data;
-  // Render the product data in your template
+const renderProducts = (products) => {
   const productContainer = document.querySelector(".row");
+  let html = "";
   products.forEach((product) => {
-    console.log(product.main_image);
-    const productCard = document.createElement("div");
-    productCard.className = "col-md-4";
-    productCard.innerHTML = `
-        <div class="card mt-2">
-          <img class="card-img-top" src="${
-            product.main_image
-          }" alt="img" width="50" height="400">
-          <div class="card-body">
-            <h5 class="card-title"><a class="link-dark" href="#">${
-              product.title
-            }</a></h5>
-            <p class="card-text">${product.description.slice(0, 50)}...</p>
+    html += `
+        <div class="col-md-4">
+          <div class="card mt-2">
+            <img class="card-img-top" src="${
+              product.main_image
+            }" alt="img" width="50" height="400">
+            <div class="card-body">
+              <h5 class="card-title"><a class="link-dark" href="#">${
+                product.title
+              }</a></h5>
+              <p class="card-text">${product.description.slice(0, 50)}...</p>
+            </div>
           </div>
         </div>
       `;
-    productContainer.appendChild(productCard);
+  });
+  productContainer.innerHTML = html;
+};
+
+const categories = ["all", "women", "men", "accessories"];
+// Call the fetchData() function
+const initializePage = async () => {
+  if (!categories.includes(category)) console.log("category not found");
+  const data = await fetchDataBycategory(category);
+  renderProducts(data.data);
+};
+initializePage();
+
+categories.forEach((category) => {
+  const catLink = document.getElementById(`${category}-link`);
+  catLink.addEventListener("click", async (e) => {
+    e.preventDefault();
+    const data = await fetchDataBycategory(category);
+    renderProducts(data.data);
   });
 });
