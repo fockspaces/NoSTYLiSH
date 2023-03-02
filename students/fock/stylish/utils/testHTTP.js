47b650eb218e2452250160cb550a30f6f88a843e
Fock Space <a86gj3sp4@gmail.com>
2023-03-02 19:37:23 +0800

    [npm] modify the npm test script

--- students/fock/stylish/utils/testHTTP.js
+++ students/fock/stylish/utils/testHTTP.js
@@ -1,10 +1,10 @@
 const axios = require("axios");
-const domain = "localhost";
+const domain = "52.194.142.24";
 const endpoint = `http://${domain}/api/1.0/products/all`;
 
 let hit = 0;
 let miss = 0;
-const delay = 10;
+const delay = 0;
 const ipAddresses = [
   "127.0.0.1",
   "192.168.1.1",
@@ -20,13 +20,13 @@ const endpoints = [
 ];
 
 const token =
-  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwiZW1haWwiOiIxMjMzMjEzQHF3ZW0uY29tIiwicm9sZV9pZCI6MiwiaWF0IjoxNjc3NzUwNjg0LCJleHAiOjE2Nzc4MzcwODR9._mAOZKKYJWakIPCr7jRW9HrF9OCdaJqPngXQ6HPxlvk";
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiZW1haWwiOiIxMjMzMjEzQHF3ZW0uY29tIiwicm9sZV9pZCI6MiwiaWF0IjoxNjc3NzUzNTU5LCJleHAiOjE2Nzc4Mzk5NTl9.i64y-6fIe8noUxx01TRoXasARN90O3e2hXxmSXWSb68";
 
 const messages = [];
 
 const test = async () => {
   console.time("Total time");
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 100; i++) {
     try {
       const res = await axios.get(endpoint);
       hit++;
@@ -42,18 +42,20 @@ const test = async () => {
 };
 
 const test2 = async () => {
+  console.time("Total time");
+
   const results = await Promise.all(
     ipAddresses.map(async (ip, idx) => {
-      for (let i = 0; i < 50; i++) {
+      for (let i = 0; i < 100; i++) {
         try {
           const res = await axios.get(endpoint, {
             headers: { "X-Forwarded-For": ip },
           });
           hit++;
-          console.log(`[${idx}] Request ${i} succeeded`);
+          // console.log(`[${idx}] Request ${i} succeeded`);
         } catch (e) {
           miss++;
-          console.error(`[${idx}] Request ${i} failed`);
+          // console.error(`[${idx}] Request ${i} failed`);
         }
         await new Promise((resolve) => setTimeout(resolve, delay));
       }
@@ -63,6 +65,7 @@ const test2 = async () => {
     })
   );
 
+  console.timeEnd("Total time");
   messages.forEach((message) => console.log(message));
   const totalHits = results.reduce((acc, { hit }) => acc + hit, 0);
   const totalMisses = results.reduce((acc, { miss }) => acc + miss, 0);
@@ -71,42 +74,58 @@ const test2 = async () => {
 };
 
 const test3 = async () => {
+  console.time("Total time");
+
   const hitCount = new Map();
   const missCount = new Map();
 
   // Send 100 requests from different IP addresses to each endpoint
   for (const endpoint of endpoints) {
-    const ip = `192.168.1.${Math.floor(Math.random() * 255)}`;
+    const ipCount = new Map();
 
-    for (let i = 0; i < 100; i++) {
-      try {
-        const res = await axios.get(endpoint, {
+    for (const ipAddress of ipAddresses) {
+      ipCount.set(ipAddress, { hit: 0, miss: 0 });
+    }
+
+    for (let i = 0; i < 1000; i++) {
+      const ip = ipAddresses[Math.floor(Math.random() * ipAddresses.length)];
+      const request = axios
+        .get(endpoint, {
           headers: { "X-Forwarded-For": ip, Authorization: token },
+        })
+        .then((res) => {
+          const endpointData = hitCount.get(endpoint) || new Map();
+          hitCount.set(endpoint, endpointData);
+
+          const ipData = endpointData.get(ip) || { hit: 0, miss: 0 };
+          ipData.hit++;
+          endpointData.set(ip, ipData);
+        })
+        .catch((e) => {
+          const endpointData = missCount.get(endpoint) || new Map();
+          missCount.set(endpoint, endpointData);
+
+          const ipData = endpointData.get(ip) || { hit: 0, miss: 0 };
+          ipData.miss++;
+          endpointData.set(ip, ipData);
         });
-
-        if (!hitCount.has(endpoint)) hitCount.set(endpoint, 0);
-        hitCount.set(endpoint, hitCount.get(endpoint) + 1);
-
-        console.log(` ${endpoint.slice(-12, -1)} request ${i} succeeded`);
-      } catch (e) {
-        if (!missCount.has(endpoint)) missCount.set(endpoint, 0);
-        missCount.set(endpoint, missCount.get(endpoint) + 1);
-
-        console.log(` ${endpoint.slice(-12, -1)} request ${i} failed`);
-      }
 
       // Add a random delay between requests to simulate concurrent requests
       await new Promise((resolve) => setTimeout(resolve, Math.random() * 100));
     }
   }
 
-  // Print the hit and miss count for each endpoint
-  for (const [endpoint, hit] of hitCount.entries()) {
-    const miss = missCount.get(endpoint) || 0;
-    console.log(` ${endpoint.slice(-12, -1)} hit rate: ${hit} / ${hit + miss}`);
+  console.timeEnd("Total time");
+
+  // Print the hit and miss count for each endpoint and IP address
+  for (const [endpoint, endpointData] of hitCount.entries()) {
+    console.log(`Endpoint: ${endpoint}`);
+    for (const [ip, { hit, miss }] of endpointData.entries()) {
+      console.log(`  IP: ${ip}, hit rate: ${hit} / ${hit + miss}`);
+    }
   }
 };
 
-test();
+// test();
 // test2();
-// test3();
+test3();
