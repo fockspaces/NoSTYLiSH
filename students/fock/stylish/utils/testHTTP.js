c14317ba468cea8c2f8cbe29da26ed47c81a7df5
Fock Space <a86gj3sp4@gmail.com>
2023-03-02 15:20:18 +0800

    [rateLimit] add testing

--- students/fock/stylish/utils/testHTTP.js
+++ students/fock/stylish/utils/testHTTP.js
@@ -1,5 +1,4 @@
 const axios = require("axios");
-const test = require("test");
 
 const endpoint = "http://localhost/api/1.0/products/all";
 
@@ -7,7 +6,7 @@ let hit = 0;
 let miss = 0;
 
 // way 1
-test1 = async () => {
+test = async () => {
   for (let i = 0; i < 100; i++) {
     try {
       const res = await axios.get(endpoint);
@@ -21,18 +20,5 @@ test1 = async () => {
   console.log(`hit rate : ${hit} / ${hit + miss}`);
 };
 
-test2 = async () => {
-  test("should allow requests when below the rate limit", async () => {
-    // Send 5 requests below the rate limit
-    for (let i = 0; i < 5; i++) {
-      const res = await axios.get(endpoint);
-      expect(res.status).toBe(200);
-    }
 
-    // Verify that the rate limit has not been reached
-    const res = await axios.get(endpoint);
-    expect(res.status).toBe(200);
-  });
-};
-
-test2();
+test();
